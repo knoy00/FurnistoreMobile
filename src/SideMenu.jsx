f58ecc2cb58d7af25c2cc1d846c0198b5506ca41
kelvin <kelvin.oy16@gmail.com>
2024-10-27 23:47:23 +0000

    Modified Side bar

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 
 
 function Menu(){
+    const [bedroomOpen, setbedroomOpen] = useState(false);
+    const toggleBedroom = () => {
+        setbedroomOpen(!bedroomOpen);
+    }
     return (
         <View style={styles.container}>
-            <TouchableOpacity>
-                <Icon name="Home" size={25} color="#ffffff" />
-                <Text>Home</Text>
-            </TouchableOpacity>
+            <View style={styles.menu}>
+                <TouchableOpacity style={styles.menuItem}>
+                    <Icon name="home" size={25} color="#ffffff" />
+                    <Text style={styles.text}>Home</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity></TouchableOpacity>
+                <TouchableOpacity style={styles.menuItem} onPress={toggleBedroom}>
+                    <Icon name="bed" size={25} color="#ffffff" />
+                    <Text style={styles.text}>Bed</Text>
+                    <Icon name="angle-down" size={25} color="#ffffff" style={{marginLeft: 'auto'}}/>
+                </TouchableOpacity>
+                {bedroomOpen && (
+                    <>
+                        <TouchableOpacity style={styles.dropDown}>
+                            <View style={styles.subDropdown}>
+                                <Text style={styles.dropDownText}>Queen Size</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.dropDown}>
+                            <View style={styles.subDropdown}>
+                                <Text style={styles.dropDownText}>King Size</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </>
+                    
+
+                )}
+
+
+                <TouchableOpacity style={styles.menuItem}>
+                    <Icon name="info-circle" size={25} color="#ffffff" />
+                    <Text style={styles.text}>About us</Text>
+                </TouchableOpacity>
+            </View>
+           
 
         </View>
     )
@@ -22,8 +56,45 @@ export default Menu;
 const styles = StyleSheet.create({
     container: {
         width: '80%',
-        backgroundColor: 'red',
+        backgroundColor: '#FFEEE5',
         height: 1000,
         flexDirection: 'column'
+    }, 
+    menu: {
+        backgroundColor: '#FFEEE5',
+        marginTop: 100,
+        height: 800,
+        width: '100%',
+        padding: 20
+    },
+    menuItem: {
+        flexDirection: 'row',
+        gap: 10,
+        alignItems: 'center',
+        width: '100%',
+        padding: 10,
+        height: 50,
+        backgroundColor: '#ff5f00',
+        borderRadius: 10,
+        marginBottom: 15
+    },
+    text: {
+        color: '#ffffff',
+        fontSize: 18
+    },
+    dropdown: {
+        paddingLeft: 10,
+    },
+    subDropdown: {
+        paddingLeft: 10,
+        paddingTop: 0,
+    },
+    dropDownText: {
+        fontSize: 20,
+        paddingVertical: 0,
+        color: '#ff5f00',
+        paddingLeft: 0,
+        textAlign: 'left',
+        marginBottom: 10
     }
 });
