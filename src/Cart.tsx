dcc71333002f0ac67087205fb0f20023c383fef4
kelvin <kelvin.oy16@gmail.com>
2024-10-31 22:03:42 +0000

    Modified Cart Component

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Cart = ({ route }) => {
   const { cart } = route.params;
+
+  const [quantity, setQuantity] = useState(1)
+
 
   return (
     <>
@@ -11,11 +15,33 @@ const Cart = ({ route }) => {
           <Text>Cart is Empty uno! Brokie</Text>
         </View>
       ) : (
-        <ScrollView contentContainerStyle={styles.cartContainer}>
-          {cart.map((cartItem, index) => (
-            <Text key={index}>{cartItem.name}</Text>
-          ))}
-        </ScrollView>
+        <>
+            <View style={styles.cartHeader}>
+              <Text style={{ color: "white", fontSize: 25, textAlign: 'center', fontWeight: 'bold' }}>Cart</Text>
+            </View>
+            <ScrollView contentContainerStyle={styles.cartContainer}>
+              {cart.map((cartItem, index) => (
+                <View style={styles.itemContainer}>
+                    <View>
+                        <Image source={cartItem.image} style={{ width: 100, height: 100 }} />
+                    </View>
+                    <View style={styles.itemRight}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 20}}>{cartItem.name}</Text>
+                        <Text style={{ fontWeight: 'bold', fontSize: 15, color: 'grey'}}>Quantity:{} </Text>
+
+                        <View  style={styles.cartPrice_flex}>
+                            {cartItem.discount ? <Text style={styles.price}>GH₵{parseInt(cartItem.price) - parseInt(cartItem.discount)/100 * parseInt(cartItem.price)}</Text> : <Text style={styles.price}>GH₵{cartItem.price}</Text>}
+                            <View style={styles.buttons}>
+                                <AntDesign name="minuscircle" size={25} color="#ff5f00" />
+                                <Text>{0}</Text>
+                                <AntDesign name="pluscircle" size={25} color="#ff5f00" />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+              ))}
+            </ScrollView>
+        </>
       )}
     </>
   );
@@ -32,4 +58,40 @@ const styles = StyleSheet.create({
   cartContainer: {
     padding: 10,
   },
+  cartHeader: {
+    backgroundColor: "#ff5f00",
+    padding: 20,
+    marginTop: 30,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    padding: 15
+  },
+  itemRight:{
+    backgroundColor:'red',
+    width: '70%',
+    height: '100%',
+    padding: 10
+  },
+  cartPrice_flex: {
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: '50%',
+  },
+  buttons: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  price: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#000000',
+   
+    marginTop: 5,
+  }
 });
